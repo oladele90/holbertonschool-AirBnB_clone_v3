9c70f9f16939f174e940db20413d6a3528b81880
evanrich2404 <evanrich2404@gmail.com>
2023-04-03 17:40:52 -0500

    404 is found

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """handles 404 errors that are not found"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
