f7ea141f3a98be3d3631018f0d619d5ec9f41e41
evanrich2404 <evanrich2404@gmail.com>
2023-04-03 17:34:58 -0500

    task 5, really just adding a route to show stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,14 @@ from flask import jsonify
 def status():
     """returns a JSON status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    from models import storage
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
