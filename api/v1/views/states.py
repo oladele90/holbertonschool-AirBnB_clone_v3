5049297be276f77e284db63b57dce15adeb51319
oladele90 <oladelemiro07@gmail.com>
2023-04-04 10:02:14 -0700

    updates get function in db storage file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """states.py"""
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response, Flask
 from models import storage
 from models.state import State
 
@@ -9,14 +9,14 @@ from models.state import State
 @app_views.route('/states', strict_slashes=False)
 def states():
     """retrieves the list of all State objects"""
-    states = storage.all("State")
+    states = storage.all(State)
     return jsonify([state.to_dict() for state in states.values()])
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def state(state_id):
     """retrieves a State object"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
@@ -26,12 +26,12 @@ def state(state_id):
                  strict_slashes=False)
 def delete_state(state_id):
     """deletes a State object"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     state.delete()
     storage.save()
-    return jsonify({})
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
@@ -43,13 +43,13 @@ def post_state():
         abort(400, "Missing name")
     state = State(**request.get_json())
     state.save()
-    return jsonify(state.to_dict()), 201
+    return make_response(jsonify(state.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """updates a State object"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     if not request.get_json():
@@ -58,4 +58,4 @@ def put_state(state_id):
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
     state.save()
-    return jsonify(state.to_dict()), 200
+    return make_response(jsonify(state.to_dict()), 200)
