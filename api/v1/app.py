f4aeefaabb889b23b6813ea4f976f82ac70901f6
oladele90 <oladelemiro07@gmail.com>
2023-04-04 15:39:02 -0700

    updates to allow CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,17 +4,13 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
-
-@cross_origin(origin="*")
-def temp():
-    pass
 
 @app.teardown_appcontext
 def teardown_db(exception):
