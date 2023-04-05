3e03feeefed463caa21fa8ec307fe73af377ffc8
oladele90 <oladelemiro07@gmail.com>
2023-04-04 18:37:56 -0700

    remove flask import from user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """users.py"""
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response, Flask
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.user import User
 
